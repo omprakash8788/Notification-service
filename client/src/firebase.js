e923710a4cdff49a4ca33058cdfad98e15cb5b55
omprakash8788 <omprakashkumar552@gmail.com>
2026-06-26 21:06:35 +0530

    adding notify api

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -24,12 +24,12 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCABn6tD_MOAnW8dlsRSBM7Wc4YLkjxu_Q",
-  authDomain: "notification-platform-a0d98.firebaseapp.com",
-  projectId: "notification-platform-a0d98",
-  storageBucket: "notification-platform-a0d98.firebasestorage.app",
-  messagingSenderId: "441933183160",
-  appId: "1:441933183160:web:5884ea6c4976887c105c4d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
